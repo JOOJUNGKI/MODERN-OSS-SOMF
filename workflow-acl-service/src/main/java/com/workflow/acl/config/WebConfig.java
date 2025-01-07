20dd3449dab10e53fee9aa0c9fb4040fa1515de7
SuHo Kim <is_null@kakao.com>
2025-01-07 16:21:45 +0900

    .

--- workflow-acl-service/src/main/java/com/workflow/acl/config/WebConfig.java
+++ workflow-acl-service/src/main/java/com/workflow/acl/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.workflow.acl.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -19,6 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("favicon.ico")
-                .addResourceLocations("classpath:/static/");
+                .addResourceLocations("classpath:/static/")
+                .setCacheControl(CacheControl.noCache());
     }
 }
